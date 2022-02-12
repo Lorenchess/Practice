5c90191c1aac99432ebaa5ca691384dea2ec41cc
Ramon Lorente <ramon_lorente@yahoo.com>
2022-02-12 18:15:51 -0500

    Complete Javanais

--- src/Javanais/Main.java
+++ src/Javanais/Main.java
@@ -6,25 +6,23 @@ public class Main {
          * Before each of the vowels a, e, i, o, u insert the parasitic syllable "av";
          * Unless the vowel is preceded by another vowel.
          **/
-        translate("This is an example for you");
+        System.out.println(translate("This is an example for your employee"));
+        //System.out.println(isVowel('A'));
+    }
+
+    public static boolean isVowel(char c) {
+        return "aeiou".contains(String.valueOf(c).toLowerCase());
     }
 
     public static String translate(String text) {
-        char[] vowels= new char[] {'a', 'e', 'i', 'o', 'u'};
-        String parasitic = "av";
-        String o= "o";
-        for (int i = 0; i < text.length();i++) {
-            for (int j = i; j < vowels.length-1; j++){
-                if (text.charAt(i) == vowels[j] && text.charAt(i + 1) != vowels[j]) {
-                    String text1 = text.replace(o, "avo");
-                    System.out.println(text1);
-                    return "parasitic";
-                }
-            }
-
-
-        }
-        System.out.println(text);
-        return "no parasitic";
+      boolean isPreviousVowel = false;
+      String result = "";
+      for (int i =0; i < text.length(); i++) {
+          if (isVowel(text.charAt(i)) &&  !isPreviousVowel )
+              result += "av";
+          result += text.charAt(i);
+          isPreviousVowel = isVowel(text.charAt(i));
+      }
+      return result;
     }
 }
